9c2175fb8166844558b61986b020d41100b2ea37
nyugoh <nyugoh@gmail.com>
2019-04-28 00:32:30 +0300

    Changed Nadab fee to 0.0099 of the orders

--- src/controllers/Payment.js
+++ src/controllers/Payment.js
@@ -3,13 +3,13 @@ const Payment = require('../models/Payment');
 const API_BASE = process.env.API_BASE_URL;
 
 exports.pay = async (req, res) => {
-  console.log(req.body);
   const payment = new Payment(req.body);
   payment.hotel = req.params.hotelId;
   try {
     if (payment.total <= 0) throw new Error('Amount must be greater than 0;');
     await payment.save();
   } catch (e) {
+    req.flash('success', { msg: `${payment.total} Payment added successfully` });
     res.json({ success: false, message: e.message });
   }
   res.redirect(`/hotels/${req.params.hotelId}`);
