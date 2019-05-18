6691cc0c7d5719e3ffe54ac5df06adba743a1413
nyugoh <nyugoh@gmail.com>
2019-05-18 08:10:56 +0000

    Fixed merge conflict

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ mongoose.connection.on('error', (err) => {
  * Express configuration.
  */
 app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
-app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 5050);
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'pug');
 app.use(compression());
@@ -79,7 +79,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
   store: new MongoStore({
-    url: process.env.MONGODB_DEV,
+    url: process.env.MONGODB_URI,
     autoReconnect: true,
   })
 }));
--- nadab.js
+++ nadab.js
@@ -10,9 +10,9 @@ const config = require('./config/config');
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
-mongoose.connect(process.env.MONGODB_DEV);
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('error', (err) => {
-  console.error(err);
+  console.error(err.message);
   console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
   process.exit();
 });
